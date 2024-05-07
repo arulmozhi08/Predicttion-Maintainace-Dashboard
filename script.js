45fd38da88848fdfb6d8dcaa377f8e4c59b25ad2
arulmozhi08 <168533224+arulmozhi08@users.noreply.github.com>
2024-05-07 12:08:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
     body.classList.add(randomBgClass);
 });
 
+function redirectToAlerts(machine) {
+    window.location.href = `alerts.html?machine=${machine}`;
+}
+
+function goBack() {
+    window.history.back();
+}
+
 function getRandomBackgroundClass() {
     const backgrounds = ['background1', 'background2', 'background3']; // Add more background classes if needed
     const randomIndex = Math.floor(Math.random() * backgrounds.length);
     return backgrounds[randomIndex];
 }
-
